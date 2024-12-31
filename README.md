72edde4cacd9a0bcd3c2778d8cbf2b4441611655
Bharatha De Silva <bharathanet@gmail.com>
2024-12-31 19:27:27 +0530

    Add doc tests and updated ReadMe.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Expression Engine Project
+
+#### Usage 
+
+```rust
+use expression_engine::{parse, ExpressionNode, ExpressionNodeType, Operator};
+use std::f32::consts::PI;
+
+const EPSILON: f64 = 0.000000001;
+
+let test = |exp_str: &str, input: f64| -> f64 {
+    parse(exp_str).unwrap().evaluate("x", input).unwrap()
+};
+
+assert_eq!(test("x", 1.5), 1.5);
+assert_eq!(test("x^2", 1.5), 2.25);
+assert_eq!(test("x^2 + x^3", 1.5), 5.625);
+assert!((test("sin x", PI as f64 / 2.0) - 1.0).abs() < EPSILON);
+```
--- expression_engine/src/parser.rs
+++ expression_engine/src/parser.rs
@@ -3,6 +3,28 @@ use crate::operator::*;
 use crate::token::*;
 use crate::trigonometric::TrigonometricFunction;
 
+///
+/// Parse text expression into expression object
+///
+/// # Examples
+///
+/// ```
+/// use expression_engine::{parse, ExpressionNode, ExpressionNodeType, Operator};
+/// use std::f32::consts::PI;
+///
+/// const EPSILON: f64 = 0.000000001;
+///
+/// let test = |exp_str: &str, input: f64| -> f64 {
+///     parse(exp_str).unwrap().evaluate("x", input).unwrap()
+/// };
+///
+/// assert_eq!(test("x", 1.5), 1.5);
+/// assert_eq!(test("x^2", 1.5), 2.25);
+/// assert_eq!(test("x^2 + x^3", 1.5), 5.625);
+/// assert!((test("sin x", PI as f64 / 2.0) - 1.0).abs() < EPSILON);
+///
+///  ```
+///
 pub fn parse(expression: &str) -> Result<ExpressionNode, ParseError> {
     let tokens = tokenize(expression)?;
     let tokens_ref = tokens.as_slice();
